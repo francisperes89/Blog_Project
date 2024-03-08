03d4afff7165f6d45df0230865e5a9cfe998b661
francisperes89 <francis.po@hotmail.com>
2024-03-08 21:10:53 +0000

    Add login authentication, login form

--- forms.py
+++ forms.py
@@ -21,7 +21,11 @@ class RegisterForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     submit = SubmitField('Sign me up')
 
-# TODO: Create a LoginForm to login existing users
 
+# Create a LoginForm to login existing users
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Let me in!')
 
 # TODO: Create a CommentForm so users can leave comments below posts
--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import datetime
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from flask_ckeditor import CKEditor
 from werkzeug.security import generate_password_hash, check_password_hash
-from forms import NewPost, RegisterForm
+from forms import NewPost, RegisterForm, LoginForm
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap5(app)
 ckeditor = CKEditor(app)
+
+# Configure Flask-Login's Login Manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+# Create a user_loader callback
+@login_manager.user_loader
+def load_user(user_id):
+    return db.get_or_404(User, user_id)
+
+
 # CONNECT TO DB
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 db = SQLAlchemy()
@@ -28,7 +41,7 @@ class BlogPost(db.Model):
     img_url = db.Column(db.String(250), nullable=False)
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(250), unique=True, nullable=False)
     name = db.Column(db.String(250), nullable=False)
@@ -43,13 +56,13 @@ with app.app_context():
 def get_all_posts():
     result = db.session.execute(db.select(BlogPost))
     posts = result.scalars().all()
-    return render_template("index.html", all_posts=posts)
+    return render_template("index.html", all_posts=posts, logged_in=current_user.is_authenticated)
 
 
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
     requested_post = db.get_or_404(BlogPost, post_id)
-    return render_template("post.html", post=requested_post)
+    return render_template("post.html", post=requested_post, logged_in=current_user.is_authenticated)
 
 
 @app.route('/new-post', methods=['GET', 'POST'])
@@ -69,7 +82,7 @@ def add():
             db.session.add(new_post)
             db.session.commit()
         return redirect(url_for('get_all_posts'))
-    return render_template('make-post.html', form=form)
+    return render_template('make-post.html', form=form, logged_in=current_user.is_authenticated)
 
 
 @app.route('/edit-post/<int:post_id>', methods=['GET', 'POST'])
@@ -90,7 +103,7 @@ def edit_post(post_id):
         post.body = edit_form.body.data
         db.session.commit()
         return redirect(url_for('show_post', post_id=post.id))
-    return render_template('make-post.html', form=edit_form, is_edit=True)
+    return render_template('make-post.html', form=edit_form, is_edit=True, logged_in=current_user.is_authenticated)
 
 
 @app.route('/delete/<int:post_id>')
@@ -106,6 +119,15 @@ def delete(post_id):
 def register():
     register_form = RegisterForm()
     if register_form.validate_on_submit():
+        # Check if user email is already present in the database
+        email = register_form.email.data
+        result = db.session.execute(db.select(User).where(User.email == email))
+        user = result.scalar()
+        if user:
+            # User already exists
+            flash('You have already signed up with that email, log in instead!')
+            return redirect(url_for('login'))
+        # Creating new user
         new_user = User(
             email=register_form.email.data,
             password=generate_password_hash(register_form.password.data, method='pbkdf2:sha256', salt_length=8),
@@ -113,28 +135,48 @@ def register():
         )
         db.session.add(new_user)
         db.session.commit()
+        # Authenticate the user with Flask-Login
+        login_user(new_user)
         return redirect(url_for('get_all_posts'))
-    return render_template('register.html', form=register_form)
+    return render_template('register.html', form=register_form, logged_in=current_user.is_authenticated)
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", logged_in=current_user.is_authenticated)
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", logged_in=current_user.is_authenticated)
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    pass
+    form = LoginForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        result = db.session.execute(db.select(User).where(User.email == email))
+        user = result.scalar()
+        if not user:
+            flash('This email does not exist, please try again!')
+            return redirect(url_for('login'))
+        elif not check_password_hash(user.password, password):
+            flash('Password incorrect, please try again.')
+            return redirect(url_for('login'))
+        else:
+            name = user.name
+            login_user(user)
+
+        return redirect(url_for('get_all_posts', name=name))
+    return render_template("login.html", form=form, logged_in=current_user.is_authenticated)
 
 
 @app.route("/logout")
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for('get_all_posts'))
 
 
 if __name__ == "__main__":
